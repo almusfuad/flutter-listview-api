3bd60753515a1a10df31a56c6fae382c5c8a73bb
AlmusFuad <almusfuad@gmail.com>
2022-03-13 12:58:29 +0600

    screen and route setup

---
+++ flutter_listview_api/lib/screens/listview_screen.dart
@@ -0,0 +1,16 @@
+// Import core packages to intialize app
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatefulWidget {
+  ListViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewScreen> createState() => _ListViewScreenState();
+}
+
+class _ListViewScreenState extends State<ListViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
